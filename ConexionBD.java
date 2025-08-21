d8177c886f360d1b79ae9df2ab676fb3d70df675
KARINA <u24263261@utp.edu.pe>
2025-08-21 00:34:09 -0500

    Update ConexionBD.java

--- ConexionBD.java
+++ ConexionBD.java
@@ -3,8 +3,8 @@ import java.sql.*;
 public class ConexionBD {
     public static Connection getConnection() throws Exception {
         String url = "jdbc:sqlserver://localhost:1433;databaseName=tienda_abarrotes;encrypt=false;";
-        String user = "sa"; // Cambia según tu SQL Server
-        String pass = "123456"; // Cambia la clave
+        String user = "Tomo"; 
+        String pass = "123456"; 
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         return DriverManager.getConnection(url, user, pass);
     }
